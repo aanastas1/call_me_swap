c1a64cf8a28a39eb093fe047d275a5584ca23675
Andrii Loiko <aloiko@c1r8s4.42warsaw.pl>
2026-07-10 19:40:20 +0200

    main function

--- main.c
+++ main.c
@@ -1,4 +1,30 @@
-int main(void)
+int main(int argc, char **argv)
 {
-    return 0;
+	int		*values;
+	int		n;
+	double	disorder;
+	int		*ranks;
+
+	if (argc < 2)
+		return (0);
+
+	values = NULL;
+	n = parse_args(argc, argv, &values);
+	if (n <= 0 || !values)
+		return (0);
+
+	if (!validate_ints_and_no_dups(values, n))
+		return (0);
+
+	disorder = compute_disorder_values(values, n);
+	ranks = compute_ranks(values, n);
+
+	/* дальше: build stacks a/b, стратегия, операции */
+	(void)disorder;
+	(void)ranks;
+
+	free(values);
+	free(ranks);
+	return (0);
 }
+
