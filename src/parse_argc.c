be17ac37378158dec62b82318de54f5fa0feb904
Andrii Loiko <aloiko@c1r8s4.42warsaw.pl>
2026-07-12 16:46:13 +0200

    refining the parser

--- src/parse_argc.c
+++ src/parse_argc.c
@@ -1,18 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_argc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aloiko <aloiko@student.42warsaw.pl>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/12 15:09:31 by aloiko            #+#    #+#             */
+/*   Updated: 2026/07/12 15:09:44 by aloiko           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+long	ft_atoil(const char *nptr)
+{
+	int	sign;
+	long	res;
+
+	sign = 1;
+	res = 0;
+	while (ft_isspace(*nptr))
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (ft_isdigit(*nptr))
+	{
+		res = res * 10 + (*nptr - '0');
+		nptr++;
+	}
+	return (sign * res);
+}
+
 int	is_digits(char *src)
 {
+	if (*src == '-' || *src == '+')
+		src++;
 	while (*src)
 	{
-		if (!is_digit(*src)
+		if (!ft_isdigit(*src))
 			return (0);
 		src++;
 	}
-	return (0);
+	return (1);
 }
 
 int  parse_args(int argc, char **argv, int **out_values)
 {
 	int	i;
 	int	count;
+	long tmp;
 
 	*out_values = malloc(argc * sizeof(**out_values));
 	count = 0;
@@ -22,9 +62,16 @@ int  parse_args(int argc, char **argv, int **out_values)
 		if (is_flag(argv[i]))
 			set_flag(1);
 		else if (is_digits(argv[i]))
-			(*out_values)[count++] = atoi(argv[i]);
+		{
+			tmp = ft_atoil(argv[i]);
+			if (tmp < INT_MIN || tmp > INT_MAX)
+				return (0);
+			(*out_values)[count++] = tmp;
+		}
 		else
+		{
 			return (0);
+		}
 	}
 	return (count);
 }
