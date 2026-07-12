71ad902efea0599a957ff9d564e7cb6bc6115b5e
Andrii Loiko <aloiko@c1r8s4.42warsaw.pl>
2026-07-12 18:55:08 +0200

    full parsing

--- main.c
+++ main.c
@@ -1,17 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aloiko <aloiko@student.42warsaw.pl>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/12 18:39:35 by aloiko            #+#    #+#             */
+/*   Updated: 2026/07/12 18:39:41 by aloiko           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
 int main(int argc, char **argv)
 {
 	int		*values;
 	int		n;
 	double	disorder;
 	int		*ranks;
+	t_context	context;
 
 	if (argc < 2)
 		return (0);
 
 	values = NULL;
-	n = parse_args(argc, argv, &values);
+	n = parse_args(argc, argv, &values, &context);
 	if (n <= 0 || !values)
+	{
+		ft_putstr_fd("Error\n", 2);
 		return (0);
+	}
 
 	if (!validate_ints_and_no_dups(values, n))
 		return (0);
--- src/parse_argc.c
+++ src/parse_argc.c
@@ -6,11 +6,25 @@
 /*   By: aloiko <aloiko@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/12 15:09:31 by aloiko            #+#    #+#             */
-/*   Updated: 2026/07/12 16:46:01 by aloiko           ###   ########.fr       */
+/*   Updated: 2026/07/12 18:53:14 by aloiko           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while (1)	
+	{
+		if ((unsigned char)*s1 != (unsigned char)*s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		if (!(unsigned char)*s1)
+			return (0);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
 
 long	ft_atoil(const char *nptr)
 {
@@ -48,32 +62,41 @@ int	is_digits(char *src)
 	return (1);
 }
 
-int	is_flag(char *src)
+int	is_flag(char *src, t_context *context)
 {
 	const char *flags[] = {"--simple", "--medium", "--complex", "--adaptive", "--bench", NULL};
-	while (*flags)
+	int i;
+	
+	i = 0;
+	while (flags[i])
 	{
-		if (ft_strcmp(*flags, src) == 0)
+		if (ft_strcmp(src, flags[i]) == 0)
 		{
+			if (i < 4 && context->strategy == NONE) /* strategy flags */
+				context->strategy = (t_strategy)i;
+			else if (i == 4 && context->bench_enabled == 0) /* bench flag */
+				context->bench_enabled = 1;
 			return (1);
 		}
-		flags++;
-	}	
+		i++;
+	}
 	return (0);
 }
 
-int  parse_args(int argc, char **argv, int **out_values)
+int  parse_args(int argc, char **argv, int **out_values, t_context *context)
 {
 	int	i;
 	int	count;
 	long tmp;
+	context->strategy = NONE; 
+	context->bench_enabled = 0;
 
 	*out_values = malloc(argc * sizeof(**out_values));
 	count = 0;
 	i = 0;
 	while (++i < argc)
 	{
-		if (is_flag(argv[i]))
+		if (is_flag(argv[i], context))
 			continue ;
 		else if (is_digits(argv[i]))
 		{
